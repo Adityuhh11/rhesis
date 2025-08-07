c4679cd00fd3c20c379b2ab33c76f0b4476156ef
Harry Cruz <harry@rhesis.ai>
2025-08-07 12:52:15 +0000

    Temporary CORS port

--- apps/backend/src/rhesis/backend/app/main.py
+++ apps/backend/src/rhesis/backend/app/main.py
@@ -95,7 +95,7 @@ app = FastAPI(
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://app.rhesis.ai", "https://dev-app.rhesis.ai", "https://dev-api.rhesis.ai"],
+    allow_origins=["http://localhost:3000", "http://localhost:3001", "https://app.rhesis.ai", "https://dev-app.rhesis.ai", "https://dev-api.rhesis.ai"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
